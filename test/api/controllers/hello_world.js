993e0799ea2b4ff00d6fd581b3605d220eaa0bc8
Victoria Malik De Tchara <vmt@dxc.com>
2018-01-04 14:34:33 +0000

    Commit

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -20,7 +20,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello NenuNenu!');
+            res.body.should.eql('Hello NenuNenu, stranger!');
 
             done();
           });
@@ -39,7 +39,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello NenuNenu!');
+            res.body.should.eql('Hello NenuNenu, stranger!');
 
             done();
           });
